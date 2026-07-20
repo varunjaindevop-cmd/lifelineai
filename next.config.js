4919ed7eb52f5a5532b1eab74364284efeeb067d
virendra jain <virendrajain624@gmail.com>
2026-07-20 11:02:35 +0530

    fix: exclude onnxruntime-web from Terser to fix import.meta build error

--- next.config.js
+++ next.config.js
@@ -8,6 +8,34 @@ const nextConfig = {
       },
     ],
   },
+  // Don't bundle onnxruntime-web - it has pre-minified code with import.meta
+  serverExternalPackages: ['onnxruntime-web'],
+  webpack: (config, { isServer }) => {
+    // Handle WASM files
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+    };
+
+    // Exclude ort.bundle files from Terser minification
+    // These files already use import.meta which Terser can't handle
+    if (config.optimization?.minimizer) {
+      const filteredMinimizers = [];
+      for (const minimizer of config.optimization.minimizer) {
+        if (minimizer.constructor.name === 'TerserPlugin') {
+          // Create new instance with exclude pattern
+          const opts = { ...minimizer.options };
+          opts.exclude = /ort\.bundle/;
+          filteredMinimizers.push(new minimizer.constructor(opts));
+        } else {
+          filteredMinimizers.push(minimizer);
+        }
+      }
+      config.optimization.minimizer = filteredMinimizers;
+    }
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
